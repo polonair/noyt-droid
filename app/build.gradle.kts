0c40c4744b38495dcbf0e39802244b953760d418
polonair <fixnim@gmail.com>
2026-02-25 00:46:48 +0300

    Исправить падение release resources из-за темы Material3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     androidTestImplementation(composeBom)
 
     implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
 
