1ec13ce68e1ccabcef74417576d03082554a133e
Alexander Mosyagin <alexander.mosyagin@aspose.com>
2026-02-25 13:11:08 +0300

    fix(android): use published ffmpeg-kit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
-    implementation("com.arthenica:ffmpeg-kit-full-gpl:6.0.LTS")
+    implementation("io.github.jamaismagic.ffmpeg:ffmpeg-kit-main-16kb:6.1.7")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
